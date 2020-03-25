90f84283da41c6edc23dc5eaf07fb1c341f53e1e
Đặng Ngọc Trí <ngoctri990705@gmail.com>
2020-03-25 13:41:58 +0700

    Lesson-01-teamplate

---
+++ lesson-01/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Teamplate reactjs cơ bản</h1>
+      <p>Có sử tích hợp bootstrap 4 và thư viện jquery</p>
+    </div>
+  );
+}
+
+export default App;
